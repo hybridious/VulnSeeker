0f1fe31818c9a6300aef9ce9f0180fd4a9dd0fa0
mhanne <mhanne@debian>
2014-12-09 21:44:52 +0100

    add search proto route

--- app.py
+++ app.py
@@ -1,24 +1,54 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 from flask.ext.sqlalchemy import SQLAlchemy
-import psycopg2
-import pdb
+from elasticsearch import Elasticsearch
+import psycopg2, json
+
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost/postgres'
 db = SQLAlchemy(app)
 
+import pdb
 
-sql = 'select * from vuln'
-result = db.engine.execute(sql)
-names = []
-for row in result:
-    names.append(row[1])
+@app.route('/api/search', methods=['POST'])
+def search():
+    res = []
+    es = Elasticsearch()
+    data = json.loads(request.data)
 
-print names
+    search_body = {
+        "query": {
+            "query_string": {
+                "query": data.get('search')
+            }
+        }
+    }
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+    for x in es.search(body=search_body).get('hits').get('hits'):
+        _src = x.get('_source')
+        entry = {'id' : x.get('_id')}
+        entry.update(_src)
+        res.append(entry)
+    return json.dumps(res)
+
+"""
+    #keywords = str(request.form['keywords']).split()
+    keywords = ['2014']
+    vulns= []
+    #TODO: Heavily ugly - need refactor when i'll get client need
+    for keyword in keywords:
+        print "toto2"
+        sql = "select * from vuln;"
+        print sql
+        result = db.engine.execute(sql)
+        for row in result:
+            vulns.append(row[0])
+    pdb.set_trace()
+    return jsonify(vuln=vulns)
+
+#get_alert_by_id(client_id)
+"""
 
 if __name__ == '__main__':
     app.run()
