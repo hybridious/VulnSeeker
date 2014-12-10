9bcf8ffc5027dd419ba89d822d0c0c2b5847ab46
mhanne <mhanne@debian>
2014-12-10 02:51:56 +0100

    add model.py with vulnerability schemax

---
+++ models.py
@@ -0,0 +1,25 @@
+from db import Base
+from sqlalchemy import event
+from sqlalchemy import Column, Float, String, Text
+from elasticsearch import Elasticsearch
+
+
+class Entry(Base):
+    __tablename__ = 'entries'
+    id = Column(Integer, primary_key=True)
+    cve_id = Column(String)
+    cvss_score = Column(Float)
+    version = Column(String)
+    description = Column(Text(length=500))
+    product_name = Column(String)
+
+def after_entry_insert(mapper, connection, target):
+    es = Elasticsearch()
+    es.index(index="vulnerability", doc_type="entry", id=target.id,body={"cve_id": target.cve_id, "cvss_score": target.cvss_score, "version": target.version, "description": target.description, "product_name": target.product_name})
+
+
+def after_entry_update(mapper, connection, target):
+    pass
+
+event.listen(Entry, 'after_insert', after_entry_insert)
+event.listen(Entry, 'after_update', after_entry_insert)
